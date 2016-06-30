d4c998233520ee7c7641b61ea71a6286e51a1dda
jie-wu <wujiep@uw.edu>
2016-06-30 09:41:47 -0700

    Update xsquare.c

--- xsquare.c
+++ xsquare.c
@@ -8,14 +8,14 @@ int xsquare1(int x){
 	return(x*x);
 }
 void xsquare2(int x){
-	x=x*x;
+	(*x)=(*x)*(*x);
 }
     
 
 int main(){
  int x=5;
  printf ("x=%d xsquare1=%d xsquare2=%d\n",x,xsquare1(x));
- xsquare2(x);
+ xsquare2(&x);
  printf ("xsquare2=%d\n",x);
  return 0;
 }
