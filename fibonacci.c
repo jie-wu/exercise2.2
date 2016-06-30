9654d89dd8ae2a7146795f2960b5d0881674bd94
jie-wu <wujiep@uw.edu>
2016-06-30 09:59:17 -0700

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fibonacci (int *x,int n){
- x=malloc(n*sizeof(int));
+void fibonacci (int **a,int n){
+ int *x=malloc(n*sizeof(int));
  x[0]=1;
  x[1]=1;
- for(int i=1;i<n;i++)
+ for(int i=2;i<n;i++)
   x[i]=x[i-1]+x[i-2];
+  *a=x;
 }
 
 int main(){
  int *a,k=10;
- fibonacci(a,k);
- for(int i=0;i<=k;i++){
+ fibonacci(&a,k);
+ for(int i=0;i<k;i++){
 		printf ("%d\n",a[i]);
 	}	
  free(a);
